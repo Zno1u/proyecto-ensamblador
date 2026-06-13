531e87b6404bbdbf7b1ec9cf62a0944f3159c955
Erick <erickfernandopc05@gmail.com>
2026-06-13 15:01:21 -0600

    arreglar funciones asm, ya compila.

--- procesos.asm
+++ procesos.asm
@@ -15,7 +15,7 @@ section .text
 ;   rax = total de concidencias encontradas
 ;---------------------------------------------
 
-contarCaracteres:
+contarCaracterBuscado:
     ;Calcular el total de elementos (3660)
     mov rax, rdx
     imul rax, r8
@@ -64,13 +64,20 @@ calcularPuntaje:
 ;Niveles completados. 10000 puntos
 ;El puntaje puede definirse con una fórmula sencilla. Por ejemplo, se puede dar mayor valor
 ;a las monedas recolectadas y restar puntos por la cantidad de pasos realizados.
-    mul rcx, 2000   ;Multipliamos el numero de monedas por 2000
-    mul rdx, -50    ;Multiplicamos la penalización de los pasos por -50
-    mul r8, 10000   ;Multiplicamos completar el nivel por 10000
+    imul rcx, 2000   ;Multipliamos el numero de monedas por 2000
+    imul rdx, -50    ;Multiplicamos la penalización de los pasos por -50
+    imul r8, 10000   ;Multiplicamos completar el nivel por 10000
+
     add rcx, rdx    ;(monedas * 2000) + (pasos * -50)
     add rcx, r8     ;(monedas * 2000) + (pasos * -50) + (niveles * 10000)
-    div rcx, 10     ;(monedas * 2000) + (pasos * -50) + (niveles * 10000) / 10
-    mov rax, rcx    ;Movemos el resultado a rax para retornar el puntaje
+
+    mov rax, rcx ;dividendo en rax
+    cqo         ; extiende el signo de rax a rdx:rax (para poder hacer idiv)
+    mov r9, 10
+    idiv r9
+    
+    ; div rcx, 10     ;(monedas * 2000) + (pasos * -50) + (niveles * 10000) / 10
+    ; mov rax, rcx    ;Movemos el resultado a rax para retornar el puntaje
     ret
 
 
@@ -105,8 +112,6 @@ detectarObjetoCelda:
 
 
     
-
-contarCaracteres:
 
 
 validarMovimiento:
@@ -237,7 +242,9 @@ contarCeldasLibres:
             cmp ecx, r13d ;Comprobar si llegamos a los pasos máximos (limite), ver que es r13d
             je .DejarDeContar
             
-            cmp byte [r11 - rax], "#" ;Si hay pared en la anterior, dejar de contar
+            mov r14, r11
+            sub r14, rax
+            cmp byte [r14], "#" ;Si hay pared en la anterior, dejar de contar
             je .DejarDeContar
 
             sub r11, rax
